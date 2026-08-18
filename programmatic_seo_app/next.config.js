322dd4c66af46a14f883c98fce6af64cae76c4df
Prashant Pal <prashantstoic@gmail.com>
2026-08-19 00:23:46 +0530

    Remove next.config.js

--- programmatic_seo_app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['nodemailer', 'mysql2', 'bcryptjs'],
-  },
-};
-
-module.exports = nextConfig;
